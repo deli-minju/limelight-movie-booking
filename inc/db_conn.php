f9f1c1f3a6a5ead935d760cd26dde4702f2a3c3b
minju <sw.deliminju@gmail.com>
2025-12-02 04:17:21 +0900

    🗃️ feat: DB 연결 설정 및 mysqli 연동 구현

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -1,10 +1,10 @@
 <?php
-include 'db_secrets.php';
+include_once 'inc/db_secrets.php';
 
 $conn = mysqli_connect($db_host, $db_id, $db_pw, $db_name);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "DB 연결 성공!"; // 배포 후엔 주석 처리하기
-?>
+
+mysqli_set_charset($conn, "utf8mb4");
