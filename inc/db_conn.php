d4e2e40506d7ebbe253e3b56b64f74a546f5ea32
minju <sw.deliminju@gmail.com>
2025-12-03 02:09:14 +0900

    🗃️ feat: DB 연결 설정

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -1,10 +1,10 @@
 <?php
-include_once 'inc/db_secrets.php';
+include_once __DIR__ . '/db_secrets.php';
 
 $conn = mysqli_connect($db_host, $db_id, $db_pw, $db_name);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die(json_encode(['status' => 'error', 'message' => 'DB Connection Failed']));
 }
 
 mysqli_set_charset($conn, "utf8mb4");
